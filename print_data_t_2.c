3147e3a7602199d9bd2879f8d76f1d807cc27655
toshi-uy <s_borgia@hotmail.com>
2021-03-15 18:33:59 +0000

    fixed n = 0 on print 2

--- print_data_t_2.c
+++ print_data_t_2.c
@@ -24,6 +24,9 @@ int print_octal(va_list list)
 	unsigned int n = va_arg(list, unsigned int);
 	unsigned int m = 1, aux, num = 0;
 
+	if (n == 0)
+		_putchar('0');
+	return (1);
 	while (n > 0)
 	{
 		aux = n % 8;
@@ -52,6 +55,9 @@ int print_hexa_low(va_list list)
 	char hexnum[100];
 	unsigned int decnum = va_arg(list, unsigned int);
 
+	if (decnum == 0)
+		_putchar('0');
+	return (1);
 	while (decnum != 0)
 	{
 		rem = decnum % 16;
@@ -82,6 +88,9 @@ int print_hexa_upper(va_list list)
 	char hexnum[100];
 	unsigned int decnum = va_arg(list, unsigned int);
 
+	if (decnum == 0)
+                _putchar('0');
+        return (1);
 	while (decnum != 0)
 	{
 		rem = decnum % 16;
