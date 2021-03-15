8c49b9f2d293eb4ef51cafb91fa1aee8c2ccda7c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-03-15 15:35:19 +0000

    betty corrected

--- my_main_function.c
+++ my_main_function.c
@@ -1,27 +1,23 @@
 #include "holberton.h"
 
+/**
+ * _printf - Entry point
+ * @format: string to print
+ * Return: Always 0 (Success)
+ */
 int _printf(const char *format, ...)
 {
 	data_t type[] = {
-		{"c", print_char},
-		{"s", print_char_2},
-		{"d", print_int},
-		{"i", print_int},
-		{"b", print_to_binar},
-		{"%", print_percent},
-		{"u", print_uns},
-		{"o", print_octal},
-		{"x", print_hexa_low},
-		{"X", print_hexa_upper},
-		{"S", print_str_non_print},
+		{"c", print_char}, {"s", print_char_2}, {"d", print_int},
+		{"i", print_int}, {"b", print_to_binar}, {"%", print_percent},
+		{"u", print_uns}, {"o", print_octal}, {"x", print_hexa_low},
+		{"X", print_hexa_upper}, {"S", print_str_non_print},
 		{NULL, NULL},
 	};
-
 	int indx_type, iter = 0, pos1 = 0, pos2, count = 0;
 	va_list list;
-	
-	va_start(list, format);
 
+	va_start(list, format);
 	while (format && format[iter])
 	{
 		while (format[iter] && format[iter] != '%')
@@ -31,7 +27,7 @@ int _printf(const char *format, ...)
 		count += pos2 - pos1 + 1;
 		if (format[iter])
 		{
-			pos1 = pos2 + 3;  /*need to acomodate pos2, when its more than 1 directive after %*/
+			pos1 = pos2 + 3;
 			indx_type = 0;
 			iter++;
 			while (type[indx_type].type)
@@ -48,28 +44,3 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (count);
 }
-/*
-int main()
-{
-
-    char ch = 'a';
-    char *str;
-    char *str2;
-    str2 = "wopa";
-    str = "Hola";
-	int aux = _printf("A%cB%sC%sD8\n", ch, str, str2);
-	printf ("\n");
-    printf("%d", aux);
-	printf ("\n");
-	int aux2 = _printf("alo98");
-	printf ("\n");
-    printf("%d", aux2);
-	printf ("\n");
-	int aux3 = _printf("aguada%c%s%s", ch, str, str2);
-	printf ("\n");
-    printf("%d", aux3);
-	printf ("\n");
-
-    return (0);
-}
-*/
--- print_data_t_2.c
+++ print_data_t_2.c
@@ -12,6 +12,11 @@ int print_uns(va_list list)
 	return (print_number_uns(va_arg(list, int)));
 }
 
+/**
+ * print_octal - Entry point
+ * @list: string to print
+ * Return: Always 0 (Success)
+ */
 int print_octal(va_list list)
 {
 	int i = 0, count = 0;
@@ -19,7 +24,7 @@ int print_octal(va_list list)
 	unsigned int n = va_arg(list, unsigned int);
 	unsigned int m = 1, aux, num = 0;
 
-	while(n > 0)
+	while (n > 0)
 	{
 		aux = n % 8;
 		hexnum[i] = aux;
@@ -28,27 +33,29 @@ int print_octal(va_list list)
 		n = n / 8;
 		m = m * 10;
 	}
-	for(i = i - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
-		/*printf("%i", hexnum[i]);*/
 		_putchar(hexnum[i] + '0');
 		count++;
 	}
 	return (count);
 }
 
-
-
+/**
+ * print_hexa_low - Entry point
+ * @list: string to print
+ * Return: Always 0 (Success)
+ */
 int print_hexa_low(va_list list)
 {
 	int rem, i = 0, count = 0;
 	char hexnum[100];
 	unsigned int decnum = va_arg(list, unsigned int);
 
-	while(decnum != 0)
+	while (decnum != 0)
 	{
 		rem = decnum % 16;
-		if(rem < 10)
+		if (rem < 10)
 			rem = rem + 48;
 		else
 			rem = rem + 87;
@@ -56,7 +63,7 @@ int print_hexa_low(va_list list)
 		i++;
 		decnum = decnum / 16;
 	}
-	for(i = i-1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(hexnum[i]);
 		count++;
@@ -64,16 +71,21 @@ int print_hexa_low(va_list list)
 	return (count);
 }
 
+/**
+ * print_hexa_upper - Entry point
+ * @list: string to print
+ * Return: Always 0 (Success)
+ */
 int print_hexa_upper(va_list list)
 {
 	int rem, i = 0, count = 0;
 	char hexnum[100];
 	unsigned int decnum = va_arg(list, unsigned int);
 
-	while(decnum != 0)
+	while (decnum != 0)
 	{
 		rem = decnum % 16;
-		if(rem < 10)
+		if (rem < 10)
 			rem = rem + 48;
 		else
 			rem = rem + 55;
@@ -81,7 +93,7 @@ int print_hexa_upper(va_list list)
 		i++;
 		decnum = decnum / 16;
 	}
-	for(i = i-1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(hexnum[i]);
 		count++;
@@ -89,7 +101,11 @@ int print_hexa_upper(va_list list)
 	return (count);
 }
 
-
+/**
+ * print_str_non_print - Entry point
+ * @list: string to print
+ * Return: Always 0 (Success)
+ */
 int print_str_non_print(va_list list)
 {
 	char *s_aux;
@@ -104,10 +120,10 @@ int print_str_non_print(va_list list)
 		if ((*s_aux > 0 && *s_aux < 32) || (*s_aux >= 127))
 		{
 			decnum = (int)*s_aux;
-			while(decnum != 0)
+			while (decnum != 0)
 			{
 				rem = decnum % 16;
-				if(rem < 10)
+				if (rem < 10)
 					rem = rem + 48;
 				else
 					rem = rem + 55;
@@ -117,10 +133,10 @@ int print_str_non_print(va_list list)
 			}
 			_putchar(92);
 			_putchar('x');
-			i = i -1;
-            if (i < 1)
-                _putchar('0');
-			for(; i >= 0; i--)
+			i = i - 1;
+		if (i < 1)
+			_putchar('0');
+			for (; i >= 0; i--)
 			{
 				_putchar(hexnum[i]);
 				count++;
@@ -129,7 +145,7 @@ int print_str_non_print(va_list list)
 		}
 		else
 			_putchar(*s_aux);
-    s_aux++;
+		s_aux++;
 	}
 	return (strlen(s_aux) + count);
 }
