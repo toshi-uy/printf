c8952fb5c1f93092028bcf57b493cafc74b2974f
toshi-uy <s_borgia@hotmail.com>
2021-03-16 12:57:26 +0000

    added buffer

--- aux_funcs.c
+++ aux_funcs.c
@@ -10,7 +10,19 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int;
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c!= -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
 
 /**
