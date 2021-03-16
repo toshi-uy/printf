fe5a0ae833aaa0e23f958ee1ddc4cd507bf84da5
toshi-uy <s_borgia@hotmail.com>
2021-03-16 12:33:02 +0000

    trying to fix another check on main function

--- my_main_function.c
+++ my_main_function.c
@@ -29,6 +29,8 @@ int _printf(const char *format, ...)
 		count += pos2 - pos1 + 1;
 		if (format[iter])
 		{
+			if (format[iter + 1] == '\0')
+                                return (-1);
 			pos1 = pos2 + 3;
 			indx_type = 0;
 			iter++;
